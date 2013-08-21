93d9db23e9710aff5a9f8a28e5a71618f513eadb
Jonathan Gabel <hello@jonathangabel.com>
2013-08-21 19:01:46 -0400

    added project save all test

--- test/lib/tempo/project_test.rb
+++ test/lib/tempo/project_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 describe Tempo do
   describe "Project" do
 
-    p = Tempo::Project.new
+    p = Tempo::Project.new title: 'sheep hearding'
+    p2 = Tempo::Project.new({ title: 'horticulture - basement mushrooms', tags: [ "fungi", "farming" ]})
+    p3 = Tempo::Project.new({ title: 'horticulture - backyard bonsai', tags: [ "trees", "farming", "miniaturization" ]})
 
     it "should inherit readable only id" do
       inherits_attr_read_only?(p, :id).must_equal true
@@ -17,13 +19,22 @@ describe Tempo do
       has_attr_accessor?(p, :tags).must_equal true
     end
 
+    # activate sub project when tree structure is implemented
     it "should not have accessible sub projects" do
       has_attr_accessor?(p, :sub_projects).must_equal false
     end
 
     it "should come with freeze-dry for free" do
-      p2 = Tempo::Project.new({ title: "hang-gliding", tags: [ "dangerous", "weekends" ] })
-      p2.freeze_dry.must_equal({:id=>2, :title=>"hang-gliding", :tags=>["dangerous", "weekends"]})
+      p2.freeze_dry.must_equal({:id=>2, :title=>"horticulture - basement mushrooms", :tags=>["fungi", "farming"]})
+    end
+
+    it "should save to file a collection of projects" do
+      test_file = File.join(ENV['HOME'],'.tempo','tempo_projects.yaml')
+      Tempo::Project.save_all_to_file
+      contents = eval_file_as_array( test_file )
+      contents.must_equal [ "---", ":id: 1", ":title: sheep hearding", ":tags: []",
+                            "---", ":id: 2", ":title: horticulture - basement mushrooms", ":tags:", "- fungi", "- farming",
+                            "---", ":id: 3", ":title: horticulture - backyard bonsai", ":tags:", "- trees", "- farming", "- miniaturization"]
     end
   end
 end
