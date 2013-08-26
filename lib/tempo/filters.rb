fe11a12a359879ff0fe8b339c4ccbca98968297e
Jonathan Gabel <hello@jonathangabel.com>
2013-08-26 08:27:30 -0400

    fuzzy match for tempo models

--- lib/tempo/filters.rb
+++ lib/tempo/filters.rb
@@ -5,17 +5,18 @@ module Tempo
   # that match positively against every
   # member of the matches array
 
-  def self.fuzzy_match(haystack, matches)
-    results = []
-    matches.each do |m|
-      reg = /#{m}/
-      haystack.each do |h|
-        results << h if reg.match h
-      end
-      haystack = results
-      results = []
+  def self.fuzzy_match( haystack, matches, attribute="id" )
+
+    matches = [matches] unless matches.is_a? Array
+
+    if haystack.is_a? Array
+
+      fuzzy_array_match( haystack, matches )
+
+    elsif haystack.superclass == Tempo::Model
+
+      fuzzy_model_match( haystack, matches, attribute )
     end
-    haystack
   end
 
   # Gli default behavior: When args are sent in a
@@ -29,6 +30,41 @@ module Tempo
     args.unshift flag if flag
     args.each { |a| assembled += " #{a}" }
     assembled.strip!
+  end
+
+  private
+
+  # TODO: escape regex characters ., (), etc.
+  def self.match_to_regex( match )
+    /#{match}/
+  end
+
+  def self.fuzzy_array_match( haystack, matches )
+    results = []
+    matches.each do |m|
+      reg = /#{m}/
+      haystack.each do |h|
+        results << h if reg.match h
+      end
+      haystack = results
+      results = []
+    end
+    haystack
+  end
+
+  def self.fuzzy_model_match( haystack, matches, attribute )
+    attribute = "@#{attribute}".to_sym
+    contenders = haystack.index
+    results = []
+    matches.each do |m|
+      reg = match_to_regex m
+      contenders.each do |c|
+        results << c if reg.match c.instance_variable_get(attribute)
+      end
+      contenders = results
+      results = []
+    end
+    contenders
   end
 end
 
--- test/lib/tempo/filters_test.rb
+++ test/lib/tempo/filters_test.rb
@@ -4,16 +4,34 @@ describe Tempo do
   describe "Filters" do
     describe "fuzzy match" do
 
-      haystack = ['sheep','ducks','peeping-ducks','sheep ducks']
-      match = ['ducks']
-      matches = ['ducks', 'eep']
+      describe "matching an array" do
 
-      it "should find all matches to a single match object" do
-        Tempo::fuzzy_match(haystack, match).must_equal ['ducks','peeping-ducks','sheep ducks']
+        haystack = ['sheep','ducks','peeping-ducks','sheep ducks']
+        match = 'ducks'
+        matches = ['ducks', 'eep']
+
+        it "should find all matches to a single match object" do
+          Tempo::fuzzy_match( haystack, match ).must_equal ['ducks','peeping-ducks','sheep ducks']
+        end
+
+        it "should find all matches to an array of match objects" do
+          Tempo::fuzzy_match( haystack, matches ).must_equal ['peeping-ducks','sheep ducks']
+        end
       end
 
-      it "should find all matches to a multiple match object" do
-        Tempo::fuzzy_match(haystack, matches).must_equal ['peeping-ducks','sheep ducks']
+      describe "matching a Tempo Model" do
+        it "should find all matches to a single match object" do
+          pantherinae_factory
+          matches = Tempo::fuzzy_match( Tempo::Animal, "Panthera", "genious" )
+          matches.length.must_equal 5
+        end
+
+        it "should find all matches to an array of match objects" do
+          pantherinae_factory
+          matches = Tempo::fuzzy_match( Tempo::Animal, ["p. ", "a"], "species" )
+          # "p. onca", "p. pardus" "p. zdanskyi"
+          matches.length.must_equal 3
+        end
       end
     end
 
